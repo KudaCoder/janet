a83a35dad13d5c5050d8b93be4ed11891962cc57
KudaCoder <warren.snowden@gmail.com>
2022-03-05 12:24:03 +0000

    refactor library.py

--- modules/library.py
+++ modules/library.py
@@ -8,6 +8,9 @@ import inspect
 import json
 import os
 
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
 APP_DIR = os.environ.get("APP_DIR")
 CONF_DIR = os.environ.get("CONF_DIR")
 
@@ -37,59 +40,7 @@ class AppController:
         self.conf_dir = os.path.join(self.root_dir, CONF_DIR)
         self.app_conf = os.path.join(self.conf_dir, "apps.json")
         self.app_dict = {}
-
-        apps = [
-            f
-            for f in os.listdir(self.app_dir)
-            if os.path.isfile(os.path.join(self.app_dir, f))
-        ]
-        for app in apps:
-            module_name = app.split(".py")[0]
-            module = importlib.import_module(f"modules.apps.{module_name}")
-            controllers = [
-                c for c in inspect.getmembers(module) if "Controller" in c[0]
-            ]
-            for controller in controllers:
-                app_conf = defaultdict(list)
-                command_names = controller[1].commands
-                for app_name, commands in command_names.items():
-                    app_conf[app_name] = []
-                    for command in commands:
-                        method = command["method"]
-                        for key in command["keywords"]:
-                            keyword = list(key.keys())[0]
-                            janet_input, thread_q, args, kwargs = key[keyword].values()
-                            app_conf[app_name].append(
-                                {
-                                    keyword: {
-                                        "module": module.__name__,
-                                        "_class": controller[0],
-                                        "method": method,
-                                        "janet_input": janet_input,
-                                        "thread_q": thread_q,
-                                        "args": args,
-                                        "kwargs": kwargs,
-                                    }
-                                }
-                            )
-                with open(f"{self.conf_dir}/apps/{module_name}.json", "w") as f:
-                    json.dump(app_conf, f)
-
-        for conf in os.listdir(f"{self.conf_dir}/apps"):
-            with open(f"{self.conf_dir}/apps/{conf}", "r") as f:
-                data = json.loads(f.read())
-            for k, v in data.items():
-                app_name = k
-                for keyword_data in v:
-                    keyword = list(keyword_data.keys())[0]
-                    data = keyword_data[keyword]
-                    self.add(app_name, keyword, **data)
-
-        with open(self.app_conf, "w") as fp:
-            json.dump(self.app_dict, fp)
-
-        with open(self.app_conf, "r") as apps:
-            self.app_dict = json.loads(apps.read())
+        self.update_library()
 
     def add(
         self,
@@ -143,3 +94,61 @@ class AppController:
                     func = App.from_dict(func)
 
         return func
+
+    def update_library(self):
+        apps = [
+            f
+            for f in os.listdir(self.app_dir)
+            if os.path.isfile(os.path.join(self.app_dir, f))
+        ]
+        controllers = []
+        modules = []
+        for app in apps:
+            module_name = app.split(".py")[0]
+            module = importlib.import_module(f"modules.apps.{module_name}")
+            controllers += [
+                c for c in inspect.getmembers(module) if "Controller" in c[0]
+            ]
+            modules.append(module_name)
+
+        for i, controller in enumerate(controllers):
+            app_conf = defaultdict(list)
+            command_names = controller[1].commands
+            for app_name, commands in command_names.items():
+                app_conf[app_name] = []
+                for command in commands:
+                    method = command["method"]
+                    for key in command["keywords"]:
+                        keyword = list(key.keys())[0]
+                        janet_input, thread_q, args, kwargs = key[keyword].values()
+                        app_conf[app_name].append(
+                            {
+                                keyword: {
+                                    "module": module.__name__,
+                                    "_class": controller[0],
+                                    "method": method,
+                                    "janet_input": janet_input,
+                                    "thread_q": thread_q,
+                                    "args": args,
+                                    "kwargs": kwargs,
+                                }
+                            }
+                        )
+            with open(f"{self.conf_dir}/apps/{modules[i]}.json", "w") as f:
+                json.dump(app_conf, f)
+
+        for conf in os.listdir(f"{self.conf_dir}/apps"):
+            with open(f"{self.conf_dir}/apps/{conf}", "r") as f:
+                data = json.loads(f.read())
+            for k, v in data.items():
+                app_name = k
+                for keyword_data in v:
+                    keyword = list(keyword_data.keys())[0]
+                    data = keyword_data[keyword]
+                    self.add(app_name, keyword, **data)
+
+        with open(self.app_conf, "w") as fp:
+            json.dump(self.app_dict, fp)
+
+        with open(self.app_conf, "r") as apps:
+            self.app_dict = json.loads(apps.read())
