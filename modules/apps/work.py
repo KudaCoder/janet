6614b896ef7c608d104a653550ef6c1315e17701
KudaCoder <warren.snowden@gmail.com>
2022-03-05 12:23:02 +0000

    move work utils to models

--- modules/apps/work.py
+++ modules/apps/work.py
@@ -1,9 +1,4 @@
-from modules.utils import WorkUtils
-
-from modules.storage.database import Database
-from modules.storage.models import Client
-
-import time
+from frontend.models import WorkUtils, User
 
 
 class WorkController:
@@ -38,7 +33,7 @@ class WorkController:
         ],
         "job": [
             dict(
-                method="start_job",
+                method="start_task",
                 keywords=[
                     {
                         "start": {
@@ -51,7 +46,7 @@ class WorkController:
                 ],
             ),
             dict(
-                method="stop_job",
+                method="stop_task",
                 keywords=[
                     {
                         "stop": {
@@ -67,13 +62,11 @@ class WorkController:
     }
 
     def __init__(self, speech):
-        self.db_controller = Database()
-        if self.db_controller:
-            self.db, self.user = self.db_controller.open_session()
-            self.client, self.project, self.job = self.db_controller.return_active()
-
         self.speech = speech
         self.w_utils = WorkUtils(self.speech)
+
+        self.user = User.current_user()
+        self.client, self.project, self.task = self.w_utils.return_active()
 
     def start_work(self, janet_command=None):
         if self.client:
@@ -81,52 +74,38 @@ class WorkController:
             return
 
         if self.speech.filter_response(janet_command, keywords=["for"]):
-            client_name = janet_command.split("for ")[1]
-            self.client = self.db.query(Client).filter_by(name=client_name).first()
+            client_name = " ".join(janet_command).split("for ")[1]
+            self.client = self.w_utils.find_client(name=client_name)
         if not self.client:
-            self.client = self.w_utils.which_client(self.user, self.db)
-        if self.client:
-            self.client.is_active = True
-            self.client.save(self.db)
+            self.client = self.w_utils.which_client()
+        if self.client and not self.client.is_active:
+            self.w_utils.toggle_active_client(self.client.id)
 
         return f"The active client is {self.client.name}"
 
     def stop_work(self):
-        if self.job:
-            if self.job.is_active:
-                self.stop_job()
-        if self.client:
-            self.client.is_active = False
-            self.client.save(self.db)
+        if self.client and self.client.is_active:
+            self.w_utils.toggle_active_client(self.client.id)
         if self.project and self.project.is_active:
-            self.project.is_active = False
-            self.project.save(self.db)
-        if self.job and self.job.is_active:
-            self.stop_job()
+            self.w_utils.toggle_active_project(self.project.id)
+        if self.task and self.task.is_active:
+            self.stop_task()
 
         return "Work stopped!"
 
-    def start_job(self):
-        if not self.job:
-            self.job = self.w_utils.which_job(self.user, self.client, self.db)
-        if self.job:
-            self.job.is_active = True
-            self.job.save(self.db)
-
-            self.start_time = time.time()
-            return f"Logging hours for {self.job.title}"
+    def start_task(self):
+        if not self.task:
+            self.task = self.w_utils.which_task()
+        if self.task:
+            self.w_utils.start_task(self.task)
+            return f"Logging hours for {self.task.title}"
 
         return "Sorry, there was an issue starting the job!"
 
-    def stop_job(self):
-        end_time = time.time()
-        total_time = end_time - self.start_time
-        hours = round(total_time * 2) / 2
-        if hours < 0.25:
-            hours = 0.5
+    def stop_task(self):
+        if not self.task:
+            return "No active task!"
 
-        self.job.hours = hours
-        self.job.is_active = False
-        self.job.save(self.db)
+        self.w_utils.stop_task(self.task)
 
-        return f"Stopping logging hours for {self.job.title}"
+        return f"Stopping logging hours for {self.task.title}"
--- modules/utils.py
+++ modules/utils.py
@@ -1,110 +1,12 @@
-from modules.storage.utils import create_new_client, create_new_job, create_new_project
-from modules.storage.models import Client, Job, Project
-
-from word2number import w2n
+import matplotlib.pyplot as plt
 
 
-class WorkUtils:
-    def __init__(self, speech):
-        self.speech = speech
+def show_graph(readings):
+    temp = [r.get("temp") for r in readings]
+    time = [r.get("time") for r in readings]
 
-    def which_client(self, user, db):
-        response = None
-        while True:
-            self.speech.speak("Which client are we working for?")
-            response = self.speech.listen()
-            if self.speech.filter_response(response, keywords=["stop"]):
-                return
-
-            if self.speech.filter_response(response, keywords=[""]):
-                continue
-
-            client_name = response
-            active_client = db.query(Client).filter_by(name=client_name).first()
-            if active_client is None:
-                self.speech.speak(f"Do you want to add new client {client_name}")
-                response = self.speech.listen()
-                if self.speech.filter_response(response, keywords=["stop"]):
-                    return
-
-                if not self.speech.filter_response(
-                    response, keywords=["ye", "yes", "yeah", "add", "new"]
-                ):
-                    self.speech.speak("Ok")
-                    continue
-
-                active_client = create_new_client(client_name, user.id, db)
-            if active_client:
-                break
-
-        return active_client
-
-    def which_job(self, user, client, db):
-        response = None
-        job = None
-        project = None
-        while True:
-            self.speech.speak("What job are we working on?")
-            response = self.speech.listen()
-            if self.speech.filter_response(response, keywords=["stop"]):
-                return
-
-            if self.speech.filter_response(response, keywords=[""]):
-                self.speech.speak("Sorry, didn't get that!")
-                continue
-
-            job_name = response
-            job = db.query(Job).filter(Job.title.contains(job_name)).first()
-            if not job:
-                self.speech.speak(f"Do you want to start new job {job_name}")
-                response = self.speech.listen()
-                if self.speech.filter_response(response, keywords=["stop"]):
-                    return
-
-                if not self.speech.filter_response(
-                    response, keywords=["ye", "yes", "yeah", "start", "new"]
-                ):
-                    self.speech.speak("Ok")
-                    continue
-
-                job = create_new_job(job_name, client.id, user.id, db)
-            if job:
-                break
-
-        if job and not job.project_id:
-            while True:
-                self.speech.speak("Which project do you want to assign this job too?")
-                response = self.speech.listen()
-                if self.speech.filter_response(response, keywords=["stop"]):
-                    return
-
-                project_name = response
-                project = (
-                    db.query(Project)
-                    .filter(Project.name.contains(project_name))
-                    .first()
-                )
-
-                if self.speech.filter_response(response, keywords=["new", "project"]):
-                    self.speech.speak("What name do you want to give this project?")
-                    project_name = self.speech.listen()
-
-                self.speech.speak(
-                    f"That's a name of {project_name}. Shall I save this project?"
-                )
-                response = self.speech.listen()
-                if self.speech.filter_response(
-                    response, keywords=["ye", "yes", "yeah", "add", "new"]
-                ):
-                    project = create_new_project(project_name, client.id, db)
-                    project.is_active = True
-                    project.save(db)
-                    break
-
-        if job:
-            if project:
-                job.project_id = project.id
-            job.save(db)
-            return job
-
-        return
+    plt.plot(time, temp)
+    plt.title("Habitat Temperature Readings")
+    plt.xlabel("Time (5 seconds)")
+    plt.ylabel("Temperature (Deg C)")
+    plt.show()
