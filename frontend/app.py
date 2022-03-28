04a4981a4ec198d423881819b0fbe3666ec54d81
KudaCoder <warren.snowden@gmail.com>
2022-03-28 23:59:54 +0100

    add template datetime pretty template filter

--- frontend/app.py
+++ frontend/app.py
@@ -1,8 +1,10 @@
-from flask import Flask
-
-from .config import Config
 from frontend.models import migrate, db
 from frontend import blueprints
+from .config import Config
+
+from datetime import datetime
+
+from flask import Flask
 
 
 app = Flask(__name__)
@@ -13,3 +15,22 @@ migrate.init_app(app, db)
 
 app.register_blueprint(blueprints.public.bp)
 app.register_blueprint(blueprints.habitat.bp)
+
+
+@app.template_filter()
+def pretty_dt(value):
+    day = value.day
+    return value.strftime("{D} %B, %Y @ %H:%M").replace(
+        "{D}", str(day) + ordinal_suffix(day)
+    )
+
+
+def ordinal_suffix(number):
+    if number % 10 == 1 and number != 11:
+        return "st"
+    elif number % 10 == 2 and number != 12:
+        return "nd"
+    elif number % 10 == 3 and number != 13:
+        return "rd"
+    else:
+        return "th"
