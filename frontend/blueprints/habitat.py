80ed89a9cb3a8dace095adf314a2f223e1445d35
KudaCoder <warren.snowden@gmail.com>
2022-04-05 22:11:19 +0100

    allow user input to readings graph; add current readings to home page; move time iso conversion to form

--- frontend/blueprints/habitat.py
+++ frontend/blueprints/habitat.py
@@ -4,10 +4,9 @@ import plotly.express as px
 import pandas as pd
 import plotly
 
-from datetime import datetime, time
 import json
 
-from frontend.forms import ConfigForm
+from frontend.forms import ConfigForm, ReadingForm
 from .utils import APITools
 
 bp = Blueprint("habitat", __name__, url_prefix="/habitat")
@@ -16,34 +15,42 @@ api_tools = APITools()
 
 @bp.route("/")
 def home():
-    context = {"page": "habitat"}
+    reading = api_tools.current_reading()
+
+    context = {"page": "habitat", "reading": reading}
     return render_template("habitat.html", **context)
 
 
-@bp.route("/readings/")
+@bp.route("/readings/", methods=["GET", "POST"])
 def readings():
-    readings = api_tools.find_reading_by_period(unit="minutes", time=10)
-    data = {
-        "time": [r["time"] for r in readings],
-        "temp": [r["temp"] for r in readings],
-        "hum": [r["hum"] for r in readings],
-    }
-    df = pd.DataFrame(data)
-    fig = px.line(
-        df,
-        x="time",
-        y=["temp", "hum"],
-        title="Habitat Conditions",
-    )
-    fig = fig.update_xaxes(rangeslider_visible=True)
-    fig.update_layout(width=1400, height=500)
-    habi_graph = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    habi_graph = None
+    form = ReadingForm()
+    if form.validate_on_submit():
+        data = form.data
+        readings = api_tools.find_reading_by_period(
+            unit=data["unit"], time=data["time"]
+        )
+        data = {
+            "time": [r["time"] for r in readings],
+            "temp": [r["temp"] for r in readings],
+            "hum": [r["hum"] for r in readings],
+        }
+        df = pd.DataFrame(data)
+        fig = px.line(
+            df,
+            x="time",
+            y=["temp", "hum"],
+            title="Habitat Conditions",
+        )
+        fig = fig.update_xaxes(rangeslider_visible=True)
+        fig.update_layout(width=1400, height=500)
+        habi_graph = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
 
     context = {
         "page": "habitat",
+        "form": form,
         "habitat_graph": habi_graph,
     }
-
     return render_template("readings.html", **context)
 
 
@@ -54,9 +61,6 @@ def config():
         api_tools.set_config(form.data)
 
     config = api_tools.get_config()
-    config["lights_on_time"] = time.fromisoformat(config["lights_on_time"])
-    config["lights_off_time"] = time.fromisoformat(config["lights_off_time"])
-    config["created"] = datetime.fromisoformat(config["created"])
     form.populate_form(config)
 
     context = {"page": "habitat", "form": form}
