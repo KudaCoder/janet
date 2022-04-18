92db342f905d863c2662abb817a33e99dc1ef3c4
KudaCoder <warren.snowden@gmail.com>
2022-04-18 01:11:31 +0100

    add flask-login usermixin to user class and create password methods

---
+++ frontend/migrations/versions/0282f1a7ba20_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 0282f1a7ba20
+Revises: b38f0d19c35a
+Create Date: 2022-04-17 19:31:32.510971
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '0282f1a7ba20'
+down_revision = 'b38f0d19c35a'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
--- frontend/models/user.py
+++ frontend/models/user.py
@@ -1,12 +1,16 @@
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from flask_login import UserMixin
+
 from . import db
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     __tablename__ = "user"
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(25), nullable=False, unique=True)
-    is_active = db.Column(db.Boolean, nullable=False, default=False)
+    password_hash = db.Column(db.String(150))
 
     project = db.relationship(
         "Project",
@@ -17,6 +21,13 @@ class User(db.Model):
         back_populates="worker",
     )
 
+    # For use in Janet not on website
     @staticmethod
     def current_user():
         return User.query.filter_by(is_active=True).first()
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
