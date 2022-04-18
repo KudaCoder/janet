d705aeb25ed1e31bfbe123669733f7155f922c53
KudaCoder <warren.snowden@gmail.com>
2022-04-18 01:13:50 +0100

    make graph labels prettier and add login required route to config

--- frontend/blueprints/habitat.py
+++ frontend/blueprints/habitat.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template, request
+from flask_login import login_user, login_required
 
 import plotly.express as px
 import pandas as pd
@@ -21,7 +22,7 @@ def home():
     return render_template("habitat.html", **context)
 
 
-@bp.route("/readings/", methods=["GET", "POST"])
+@bp.route("/readings", methods=["GET", "POST"])
 def readings():
     habi_graph = None
     form = ReadingForm()
@@ -31,15 +32,15 @@ def readings():
             unit=data["unit"], time=data["time"]
         )
         data = {
-            "time": [r["time"] for r in readings],
-            "temp": [r["temp"] for r in readings],
-            "hum": [r["hum"] for r in readings],
+            "Time": [r["time"] for r in readings],
+            "Temperature": [r["temp"] for r in readings],
+            "Humidity": [r["hum"] for r in readings],
         }
         df = pd.DataFrame(data)
         fig = px.line(
             df,
-            x="time",
-            y=["temp", "hum"],
+            x="Time",
+            y=["Temperature", "Humidity"],
             title="Habitat Conditions",
         )
         fig = fig.update_xaxes(rangeslider_visible=True)
@@ -49,6 +50,7 @@ def readings():
             paper_bgcolor="rgba(0, 0, 0, 0)",
             plot_bgcolor="rgba(111, 117, 128, .3)",
             title_font_size=25,
+            title_font={"family": "Josefin Sans"},
             modebar={"bgcolor": "rgba(236, 236, 236, 1)"},
         )
         habi_graph = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
@@ -62,14 +64,16 @@ def readings():
     return render_template("readings.html", **context)
 
 
-@bp.route("/config/", methods=["GET", "POST"])
+@bp.route("/config", methods=["GET", "POST"])
+@login_required
 def config():
     form = ConfigForm(request.form or None)
     if form.validate_on_submit():
         api_tools.set_config(form.data)
 
     config = api_tools.get_config()
-    form.populate_form(config)
+    if config is not None:
+        form.populate_form(config)
 
     context = {"page": "habitat", "content": "config", "form": form}
     return render_template("config.html", **context)
